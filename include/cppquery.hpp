246b734296c2b6032ddd813a9972a2f3a63ab478
Nicholas J Ingrassellino <nick@lifebloodnetworks.com>
2023-05-26 14:46:43 -0400

    Made openSocket more resilient to Blobs. Added a bunch of helper functions for sending different types of data over WebSockets. Some tweaks.

--- include/cppquery.hpp
+++ include/cppquery.hpp
@@ -1,3 +1,9 @@
+// TODO
+//	What if functions are called afer socket is deleted?
+//	What if WebSocket data received or sent is binary?
+//	Create listener for WebSocket error().
+//	https://emscripten.org/docs/porting/asyncify.html#making-async-web-apis-behave-as-if-they-were-synchronous
+
 #pragma once
 
 #ifndef __EMSCRIPTEN__
@@ -8,60 +14,69 @@
 #include <string>
 
 #include <emscripten.h>
-#include <emscripten/val.h>
-#include <emscripten/bind.h>
 
 namespace cppquery {
 	void initialize( void ) {};
 
 	namespace http {
 		void initialize( void ) {};
+
+		void fetch( const char* uri, const char* method = "GET", const char* data = NULL ) {
+			EM_ASM( {
+				async () => {
+					const response = await fetch( UTF8ToString( $0 ), {
+						method:	UTF8ToString( $1 ),
+						body:	UTF8ToString( $2 )
+					} );
+
+					return await response.text();
+				};
+			}, uri, method, data );
+		};
 	};
 
 	namespace html {
 		void initialize( void ) {};
 
-		extern "C" {
-			void createElement( const char* elementType, const char* parentQuery = "body", const char* idName = NULL ) {
-				EM_ASM( {
-					newElement = document.createElement( UTF8ToString( $0 ) );
-					document.querySelector( UTF8ToString( $1 ) ).appendChild( newElement );
+		void createElement( const char* elementType, const char* parentQuery = "body", const char* idName = NULL ) {
+			EM_ASM( {
+				newElement = document.createElement( UTF8ToString( $0 ) );
+				document.querySelector( UTF8ToString( $1 ) ).appendChild( newElement );
 
-					if ( $2 !== 0 )
-						newElement.setAttribute( "id", UTF8ToString( $2 ) );
-				}, elementType, parentQuery, idName );
-			};
+				if ( $2 !== 0 )
+					newElement.setAttribute( "id", UTF8ToString( $2 ) );
+			}, elementType, parentQuery, idName );
+		};
 
-			void createElementText( const char* parentElementId, const char* text ) {
-				EM_ASM( {
-					newElement = document.createTextNode( UTF8ToString( $1 ) );
-					document.querySelector( UTF8ToString( $0 ) ).append( newElement );
-				}, parentElementId, text );
-			};
+		void createElementText( const char* parentElementId, const char* text ) {
+			EM_ASM( {
+				newElement = document.createTextNode( UTF8ToString( $1 ) );
+				document.querySelector( UTF8ToString( $0 ) ).append( newElement );
+			}, parentElementId, text );
+		};
 
-			void setElementId( const char* query, const char* idName ) {
-				EM_ASM( {
-					document.querySelector( UTF8ToString( $0 ) ).setAttribute( "id", UTF8ToString( $1 ) );
-				}, query, idName );
-			};
+		void setElementId( const char* query, const char* idName ) {
+			EM_ASM( {
+				document.querySelector( UTF8ToString( $0 ) ).setAttribute( "id", UTF8ToString( $1 ) );
+			}, query, idName );
+		};
 
-			void addElementClass( const char* query, const char* className ) {
-				EM_ASM( {
-					document.querySelector( UTF8ToString( $0 ) ).classList.add( UTF8ToString( $1 ) );
-				}, query, className );
-			};
+		void addElementClass( const char* query, const char* className ) {
+			EM_ASM( {
+				document.querySelector( UTF8ToString( $0 ) ).classList.add( UTF8ToString( $1 ) );
+			}, query, className );
+		};
 
-			void removeElementClass( const char* query, const char* className ) {
-				EM_ASM( {
-					document.querySelector( UTF8ToString( $0 ) ).classList.remove( UTF8ToString( $1 ) );
-				}, query, className );
-			};
+		void removeElementClass( const char* query, const char* className ) {
+			EM_ASM( {
+				document.querySelector( UTF8ToString( $0 ) ).classList.remove( UTF8ToString( $1 ) );
+			}, query, className );
+		};
 
-			void appendElement( const char* elementName, const char* query ) {
-				EM_ASM( {
-					document.querySelector( UTF8ToString( $1 ) ).appendChild( document.querySelector( UTF8ToString( $0 ) ) );
-				}, elementName, query );
-			};
+		void appendElement( const char* query, const char* elementName ) {
+			EM_ASM( {
+				document.querySelector( UTF8ToString( $0 ) ).appendChild( document.querySelector( UTF8ToString( $1 ) ) );
+			}, query, elementName );
 		};
 
 		int getBodyWidth( void ) {
@@ -77,8 +92,12 @@ namespace cppquery {
 		};
 	};
 
+	namespace css {
+		void initialize( void ) {};
+	};
+
 	namespace websocket {
-		enum STATES {
+		enum STATE {
 			CONNECTING = 0,
 			OPENED,
 			CLOSING,
@@ -91,56 +110,93 @@ namespace cppquery {
 			} );
 		};
 
-		std::queue< const std::string > incomingMessages;
+		std::queue< const std::string > _incomingMessages;
 
 		EM_JS( void, javascriptWebsocketSetMessageOutbound, ( const char* message ), {
-			socket.send( UTF8ToString( message ) );
+			_websocket.send( UTF8ToString( message ) );
 		} );
 
 		extern "C" {
 			EMSCRIPTEN_KEEPALIVE
 			void cppWebsocketSetMessageInboundMessage( const char* message ) {
-				incomingMessages.push( message );
+				_incomingMessages.push( message );
 			};
 		};
 
 		int getState( void ) {
 			return EM_ASM_INT( {
-				return socket.readyState;
+				return _websocket.readyState;
 			} );
 		};
 
-		EM_JS( void, openSocket, ( const char* path ), {
-			socket = new WebSocket( UTF8ToString( path ) );
+		EM_JS( void, openSocket, ( const char* uri ), {
+			_websocket = new WebSocket( UTF8ToString( uri ) );
 
-			socket.addEventListener( "message", ( message ) => {
-				const length = lengthBytesUTF8( message.data ) + 1;
-				const cString = _malloc( length );
-				stringToUTF8( message.data, cString, length );
-				javascriptWebsocketSetMessageInboundMessage( cString );
-				_free( cString );
+			_websocket.addEventListener( "message", ( message ) => {
+				const sendMessage = ( text ) => {
+					const length = lengthBytesUTF8( text ) + 1;
+					const cString = _malloc( length );
+					stringToUTF8( text, cString, length );
+					javascriptWebsocketSetMessageInboundMessage( cString );
+					_free( cString );
+				};
+
+				if ( message.data instanceof Blob ) {
+					const reader = new FileReader();
+					reader.addEventListener( "loadend", ( event ) => {
+						sendMessage( event.srcElement.result );
+					} );
+
+					reader.readAsText( message.data );
+				}
+				else
+					sendMessage( message.data );
 			} );
 		} );
 
 		EM_JS( void, closeSocket, (), {
-			socket.close();
+			_websocket.close();
 
-			delete socket;
+			delete _websocket;
 		} );
 
 		std::size_t messagesCount( void ) {
-			return incomingMessages.size();
+			return _incomingMessages.size();
 		};
 
 		std::string getMessage( void ) {
-			const std::string message = incomingMessages.front();
-			incomingMessages.pop();
+			const std::string message = _incomingMessages.front();
+			_incomingMessages.pop();
 
 			return message;
 		};
 
-		void sendMessage( const std::string data ) {
-			javascriptWebsocketSetMessageOutbound( data.c_str() );
+		void sendMessage( const char* messsage ) {
+			javascriptWebsocketSetMessageOutbound( messsage );
+		};
+
+		void sendMessage( const std::string messsage ) {
+			sendMessage( messsage.c_str() );
+		};
+
+		void sendMessage( const int messsage ) {
+			sendMessage( std::to_string( messsage ) );
+		};
+
+		void sendMessage( const unsigned int messsage ) {
+			sendMessage( std::to_string( messsage ) );
+		};
+
+		void sendMessage( const std::size_t messsage ) {
+			sendMessage( std::to_string( messsage ) );
+		};
+
+		void sendMessage( const float messsage ) {
+			sendMessage( std::to_string( messsage ) );
+		};
+
+		void sendMessage( const double messsage ) {
+			sendMessage( std::to_string( messsage ) );
 		};
 	};
 };
